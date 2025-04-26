08f29d02a9447c51c0f71b7e5f7e4fbba9b7f5cd
muhilhams <muhilhamsyarifuddin@gmail.com>
2025-04-26 10:47:03 +0700

    update logo

--- service-worker.js
+++ service-worker.js
@@ -10,6 +10,7 @@ const urlsToCache = [
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+  "https://kit.fontawesome.com/1cc2641099.js",
   "https://sehatin.rizcasaur.us/assets/images/category-consulting.svg",
   "https://sehatin.rizcasaur.us/assets/images/category-dentist.svg",
   "https://sehatin.rizcasaur.us/assets/images/category-cardiologist.svg",
